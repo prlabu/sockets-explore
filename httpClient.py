c28bb2ca73a86ba2896660417a5d347f8245fe50
prlabu <1thisisfornothing1@gmail.com>
2019-04-15 21:36:46 +0800

    Multi-echo working, supports multiple connections. Supoorts multiple HTTP reqests per session

--- httpClient.py
+++ httpClient.py
@@ -1,11 +1,21 @@
-import requests
+import socket
 import logging
 
 # logging.basicConfig(level=logging.DEBUG)
 
-req = requests.Session()
-req.get('http://localhost:8080/flat.jpg')
-req.get('http://localhost:8080/food.mp3')
-req.get('http://localhost:8080/plainText.txt')
+req1 = "GET /plainText.txt HTTP/1.1\r\nConnection: keep-alive"
+req2 = "GET /plainText.txt HTTP/1.1\r\nConnection: keep-alive"
+req3 = "GET /plainText.txt HTTP/1.1\r\nConnection: keep-alive"
 
-req.close()
+reqs = [req1, req2, req3]
+cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cli.connect(('localhost', 8080))
+
+for req in reqs:
+    
+    cli.sendall(req.encode())
+    print(f'Req sent: \n{req}\n')
+    res = cli.recv(1024).decode()
+    print(f'Obj recv: {res}\n')
+
+cli.close()
--- multi_echo.py
+++ multi_echo.py
@@ -7,27 +7,30 @@ print(f'Server listening at port: 8080')
 socks = [server]
 reqs = {}
 ress = {}
+closes = {}
 
 class HTTPReq:
 
     def __init__(self, raw_req):
-        self.raw_req = raw_req.decode()
+        self.raw_req = raw_req
         self.res = {
             'Version': 'HTTP/1.1',
-            'Status': '',
+            'Status': None,
             'Headers': {
-                'Content-Type': ''
+                'Content-Type': '',
+                'Connection' : 'close'
             }, 
-            'Body': ''
+            'Body': '',
         }
         self.req = {
             'Type': '',
             'Version': '',
             'Path': '',
             'Headers': {
-                'Content-Type': ''
+                'Connection': 'close'
             }, 
-            'Body': ''
+            'Body': '',
+            'Unparsed': '' 
         }
 
     def get_response(self):
@@ -46,14 +49,21 @@ class HTTPReq:
 
         # parse remaining request headers, assumes no body
         for param in req_split[1:]:
-            key = param.split(':')[0].strip()
-            val = param.split(':')[1].strip()
-            self.req['Headers'][key] = val
+            try:
+                key, val = param.split(':')
+            except:
+                # print(f'Unparsed line in HTTP request: {param}')
+                self.req['Unparsed'] = param
+            else: 
+                self.req['Headers'][key.strip()] = val.strip()
+
+        # If the client specifies keep-alive, we will respect it
+        # after sending one HTTP response
+        self.res['Headers']['Connection'] = self.req['Headers']['Connection']
 
         if self.req['Type'] == 'GET':
             if self.req['Path'] == '/' or self.req['Path'] == '/favicon.ico':
                 self.req['Path'] = '/index.html'
-            
             if self.req['Path'].endswith('.jpg'):
                 self.res['Headers']['Content-Type'] = 'image/jpeg'
             elif self.req['Path'].endswith('.gif'):
@@ -72,13 +82,14 @@ class HTTPReq:
                 return self.res
             
             try:                                              
-                f = open(self.res['Path'][1:], 'rb')
+                f = open(self.req['Path'][1:], 'rb')
             except:
                 print('Requested file not found')
                 self.res['Status'] = '400 Bad Request'
                 return self.res
             else:
                 self.res['Body'] = f.read()
+                # print(f'Res body type: {type(self.res["Body"])}')
 
         elif self.req['Type'] == 'POST':
             # We would normally handle POST reqs here but the client is only requesting info here.
@@ -87,50 +98,66 @@ class HTTPReq:
             print('Invalid request type')
             self.res['Status'] = '400 Bad Request'
             return self.res
-
+        
+        self.res['Status'] = '200 OK'
         return self.res
 
 
 try: 
     while socks:
-        readable, writable, exceptional = select.select(
-            socks, socks, socks)
-        for s in readable:
-            if s is server:
-                connection, client_address = s.accept()
-                print(f'New TCP connection from: {client_address}')
-                connection.setblocking(0)
-                socks.append(connection)
-                ress[connection] = queue.Queue()
-            else:
-                data = s.recv(1024).decode()
-                if data:
-                    print(f'Message recv: {data}')
-                    reqObj = HTTPReq(data)
-                    res = reqObj.get_response() # this will be a response dictionary
-                    ress[s].put(data)
+        readable, writable, exceptional = select.select(socks, socks, socks)
+        for s in socks:
+            if s in readable:
+                if s is server:
+                    connection, client_address = s.accept()
+                    print(f'New TCP connection from: {client_address}')
+                    connection.setblocking(0)
+                    socks.append(connection)
+                    ress[connection] = queue.Queue()
                 else:
+                    data = s.recv(1024).decode()
+                    if data:
+                        # print(f'Message recv: {data}')
+                        reqObj = HTTPReq(data)
+                        res = reqObj.get_response() # this will be a response dictionary
+                        ress[s].put(res)
+                    else:
+                        print(f'No data recved from client: {s.getpeername()}')
+                        print(f'Closing connection')
+                        socks.remove(s)
+                        s.close()
+                        del ress[s]
+
+            elif s in writable:
+                try:
+                    res = ress[s].get_nowait()
+                except queue.Empty:
+                    # print('ress queue empty - nothing to send')
+                    pass
+                except KeyError:  # socket s no longer exists - connection closed by client?
                     socks.remove(s)
-                    s.close()
                     del ress[s]
+                else: # executes if no exception is caught
+                    to_send = bytearray(f'{res["Version"]} {res["Status"]}\r\n'.encode())
+                    for header, value in res['Headers'].items():
+                        to_send.extend(f'{header}: {value}\r\n'.encode())
+                    to_send.extend('\n'.encode())
+                    if res['Body']:
+                        to_send.extend(bytearray(res['Body']))
+                    s.sendall(to_send)
+                    # print(f'HTTP res sent: \n{to_send.decode()}')
+                    if res['Headers']['Connection'].lower() == 'close'.lower() or (ress[s].empty()):
+                        print(f'Closing TCP conn with: {s.getpeername()}')
+                        socks.remove(s)
+                        s.close()
+                        del ress[s]
 
-        for s in writable:
-            try:
-                res = ress[s].get_nowait()
-            except queue.Empty:
-                print('ress queue empty - nothing to send')
-            else: # executes if no exception is caught
-                to_send = bytearray(f'{res["Version"]} {res["Status"]}\r\n'.encode())
-                for header, value in res['Headers'].items():
-                    to_send.extend(f'{header}: {value}\r\n'.encode())
-                to_send.extend('\n')
-                to_send.extend(bytearray(res['Body']))
-                s.send(to_send)
-                print(f'HTTP res sent: \n{to_send.decode()}')
-
-        for s in exceptional:
-            socks.remove(s)
-            s.close()
-            del ress[s]
+            elif s in exceptional:
+                print(f'Closing TCP conn with: {s.getpeername()}')
+                socks.remove(s)
+                s.close()
+                del ress[s]
 except:
+    print('Exception raised, shutting down server')
+    server.shutdown(socket.SHUT_RDWR)
     server.close()
